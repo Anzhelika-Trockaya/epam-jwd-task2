15b3f71a75c2b803afbe6c24ea100ea46f10c478
Anzhelika-Trockaya <trockaya.anzhelika@gmail.com>
2022-03-15 12:18:44 +0300

    +

--- task2/src/test/java/by/epam/task2/parser/builder/CandiesJaxbBuilderTest.java
+++ task2/src/test/java/by/epam/task2/parser/builder/CandiesJaxbBuilderTest.java
@@ -1,96 +1,26 @@
 package by.epam.task2.parser.builder;
 
-import by.epam.task2.entity.AbstractCandy;
 import by.epam.task2.exception.ParseXMLException;
-import by.epam.task2.parser.builder.dataProvider.StaticDataProvider;
 import by.epam.task2.util.ResourcePathUtil;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Set;
-
-import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.fail;
 
-public class CandiesJaxbBuilderTest {
-    public static final String RESOURCE_NAME = "candiesTest.xml";
-    public static final String EMPTY_RESOURCE_NAME = "empty.xml";
-    public static final String INCORRECT_RESOURCE_NAME = "incorrect.xml";
-
-    CandiesJaxbBuilder jaxbBuilder;
-    Set<AbstractCandy> candies;
+public class CandiesJaxbBuilderTest extends AbstractCandiesBuilderTest {
 
     @Test
     @BeforeClass
     public void testBuildSetCandies() throws ParseXMLException {
         String fileName;
         try {
-            fileName = ResourcePathUtil.getResourcePath(RESOURCE_NAME);
+            fileName = ResourcePathUtil.getResourcePath(CORRECT_RESOURCE_NAME);
         } catch (ParseXMLException exception) {
             fail("Test failed. " + exception.getMessage());
             return;
         }
-        jaxbBuilder = new CandiesJaxbBuilder();
-        jaxbBuilder.buildSetCandies(fileName);
-        candies = jaxbBuilder.getCandies();
-    }
-
-    public AbstractCandy getCandyWithVendorCode(String vendorCode) throws ParseXMLException {
-        for (AbstractCandy candy : candies) {
-            if (candy.getVendorCode().equals(vendorCode)) {
-                return candy;
-            }
-        }
-        throw new ParseXMLException("Candy with vendorCode=" + vendorCode + " does not exist.");
-    }
-
-    @Test(dataProvider = "candies-data", dataProviderClass = StaticDataProvider.class)
-    public void testParsedData(AbstractCandy expectedCandy) throws ParseXMLException {
-        String vendorCode = expectedCandy.getVendorCode();
-        AbstractCandy actualCandy = getCandyWithVendorCode(vendorCode);
-        assertEquals(expectedCandy, actualCandy);
-    }
-
-    @Test(expectedExceptions = ParseXMLException.class, expectedExceptionsMessageRegExp = "File '.*' does not match schema '.*'")
-    public void testEmptyFile() throws ParseXMLException {
-        String fileName;
-        try {
-            fileName = ResourcePathUtil.getResourcePath(EMPTY_RESOURCE_NAME);
-        } catch (ParseXMLException exception) {
-            fail("Test failed. " + exception.getMessage());
-            return;
-        }
-        jaxbBuilder.buildSetCandies(fileName);
-    }
-
-    @Test(expectedExceptions = ParseXMLException.class, expectedExceptionsMessageRegExp = "File '.*' does not match schema '.*'")
-    public void testIncorrectFile() throws ParseXMLException {
-        String fileName;
-        try {
-            fileName = ResourcePathUtil.getResourcePath(INCORRECT_RESOURCE_NAME);
-        } catch (ParseXMLException exception) {
-            fail("Test failed. " + exception.getMessage());
-            return;
-        }
-        jaxbBuilder.buildSetCandies(fileName);
-    }
-
-    @Test(expectedExceptions = ParseXMLException.class)
-    public void testNonExistentFile() throws ParseXMLException {
-        String fileName;
-        try {
-            File nonExistent = File.createTempFile("test", "txt");
-            fileName = nonExistent.getAbsolutePath();
-            if (!nonExistent.delete()) {
-                fail("Test failed. File not deleted.");
-                return;
-            }
-        } catch (IOException ioException) {
-            fail("Test failed. " + ioException.getMessage());
-            return;
-        }
-        jaxbBuilder.buildSetCandies(fileName);
+        builder = new CandiesJaxbBuilder();
+        builder.buildSetCandies(fileName);
+        candies = builder.getCandies();
     }
 }
