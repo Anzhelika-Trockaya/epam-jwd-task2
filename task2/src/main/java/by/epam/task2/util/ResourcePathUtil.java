681e4bc1b47907ab5884efa2d134096a2f51b357
Anzhelika-Trockaya <trockaya.anzhelika@gmail.com>
2022-03-18 12:19:53 +0300

    +

--- task2/src/main/java/by/epam/task2/util/ResourcePathUtil.java
+++ task2/src/main/java/by/epam/task2/util/ResourcePathUtil.java
@@ -1,14 +1,13 @@
 package by.epam.task2.util;
 
 import by.epam.task2.exception.ParseXMLException;
-import by.epam.task2.validator.XmlFileValidator;
 
 import java.net.URL;
 
 public class ResourcePathUtil {
     public static String getResourcePath(String resourceName) throws ParseXMLException {
         final int pathStartPosition = 6;
-        ClassLoader loader = XmlFileValidator.class.getClassLoader();
+        ClassLoader loader = ResourcePathUtil.class.getClassLoader();
         URL resource = loader.getResource(resourceName);
         if (resource == null) {
             throw new ParseXMLException("Resource " + resourceName + " is not found");
